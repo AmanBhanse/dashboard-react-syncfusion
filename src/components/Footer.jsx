848f0f1a0728b1f797a599180d83c12ad78beb50
Aman Bhanse <amanbhanse22@gmail.com>
2022-08-20 10:02:45 +0530

    Implemented Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { BsTwitter, BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const iconStyling = `text-2xl`;
+
+  return (
+    <div className="flex flow-root m-4">
+      <div className="flex flex-row gap-4 m-4 float-left">
+        <a
+          href="https://github.com/AmanBhanse"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`${iconStyling}`}
+        >
+          <div>
+            <FaGithub />
+          </div>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/aman-bhanse/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`${iconStyling}`}
+        >
+          <div>
+            <BsLinkedin />
+          </div>
+        </a>
+        <a
+          href="https://twitter.com/bhanse_aman"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`${iconStyling}`}
+        >
+          <div>
+            <BsTwitter />
+          </div>
+        </a>
+      </div>
+      <div className="float-right m-4 font-semibold text-lg text-right">
+        ALL RIGHTS RESERVED @2022
+        <br />
+        Aman Bhanse
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
